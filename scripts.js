891c1768542d6d0781ecc61f8a177da74d4051b0
leinadgp <leinadgp@gmail.com>
2025-07-26 11:06:06 -0300

    Ajustado pagina do starbucks com responsividade

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+let circulo = document.querySelector(".circulo")
+let copo = document.querySelector(".copo")
+
+function trocarFundo(cor) {
+    circulo.style.background = cor
+}
+
+function trocarCopo(imagem) {
+    
+    copo.src = imagem
+
+}
